fbde96b9cf5dcfaac301be8d2759cbdc2a1290a1
Richard <richard.vieiraes@gmail.com>
2024-10-08 20:09:39 -0400

    atualização

--- Aula_06/aula 06.py
+++ Aula_06/aula 06.py
@@ -1,7 +1,51 @@
-vetor=[0,1,2,3,4,43,6,3,2]
+# vetor=[0,1,2,3,4,43,6,3,2]
+# vet=[]
 # tamanho = len(vetor)
-for i in range(1,9):
-  print(f'{i} --> {vetor[i]}')
-  # print(i,)
-  # print(" -->  ")
-  # print(vetor[i])
+# # for i in range(1,9):
+# #   print(f'{i} --> {vetor[i]}')
+# #   # print(i,)
+# #   # print(" -->  ")
+# #   # print(vetor[i])
+# print(vetor)
+# vetor.append("item")
+# vetor.insert(9,"cinco")
+# outro=vetor.pop()
+# #vetor.clear()
+# print(vetor)
+# x=0
+# while x<3:
+#   var=int(input("Entre com um numero"))
+#   vet.append(var)
+#   x+=1
+# print(vet)
+
+
+# ##### ROTINAS #####
+# def menu():
+#   print("-="*20)
+#   print("menu")
+#   print("menu")
+#   print("menu")
+#   print("menu")
+#   print("-="*20)
+
+# def clear():
+#   print("clear")
+
+# menu()
+# clear()
+
+# def depois():
+#   print("DEPOIS")
+
+
+# def soma(a,b):
+#   """
+#   essa função soma a variavel a com a B e retorna a em somar
+#   """
+#   somar=a+b
+#   return somar
+# A=10
+# B=2
+# outra=soma(23,3)
+# print(outra)
