4a688666c1c9c3d749b8bc2b0ceae8255c8f19d0
Richard <richard.vieiraes@gmail.com>
2024-10-15 20:25:07 -0400

    eexercicio vetor 1

---
+++ Aula_06/Exercicios/exvetor01.py
@@ -0,0 +1,14 @@
+#Escreva um programa que solicite ao usuário um vetor de números e exiba a soma de seus elementos.
+vetor=[]
+total=0
+condicao=False
+# for contador in range(1,5):
+while condicao!= True:
+  saida=str(input(f'Vocẽ quer adicionar mais um numero?: [S/N]'))
+  if saida in 'Ss':
+    num=(int(input('Digite um valor Inteiro: ')))
+    total+=num
+    vetor.append(num)
+  else:
+    break
+print(f'Vetor {vetor}, soma total = {total}')   
